acb425b8c94a16d32224854fc31bcb1678d6ec85
Jason <jay.jpy.wong@gmail.com>
2024-01-17 13:14:22 +0100

    Responsive burger menu added
    Responsive burger menu added still needs styling .

---
+++ scripts/script.js
@@ -0,0 +1,29 @@
+/* If the menu contains the showMenu class, we remove it to hide the menu. 
+We also toggle the display to hide the close icon and show the menu icon. */
+const menu = document.querySelector(".menu");
+const menuItems = document.querySelectorAll(".menuItem");
+const hamburger= document.querySelector(".hamburger");
+const closeIcon= document.querySelector(".closeIcon");
+const menuIcon = document.querySelector(".menuIcon");
+
+/*Javascript above, all menu items with querySelectorAll. */
+
+/* Clicking on the hamburger button will call toggleMenu(). */ 
+/* It checks if the menu contains the class showMenu. */ 
+
+function toggleMenu() {
+    /* If the menu doesn't have the showMenu class, we add it, 
+    show the close icon and hide the menu icon. */
+
+  if (menu.classList.contains("showMenu")) {
+    menu.classList.remove("showMenu");
+    closeIcon.style.display = "none";
+    menuIcon.style.display = "block";
+  } else {
+    menu.classList.add("showMenu");
+    closeIcon.style.display = "block";
+    menuIcon.style.display = "none";
+  }
+}
+
+hamburger.addEventListener("click", toggleMenu);

553c46693f62024d067d087d490ba26e75b11756
Jason <jay.jpy.wong@gmail.com>
2024-01-17 23:37:16 +0100

    Burger menu fixes
    Label added that works aswell, not done yet..

--- scripts/script.js
+++ scripts/script.js
@@ -5,6 +5,7 @@ const menuItems = document.querySelectorAll(".menuItem");
 const hamburger= document.querySelector(".hamburger");
 const closeIcon= document.querySelector(".closeIcon");
 const menuIcon = document.querySelector(".menuIcon");
+const menuLabel = document.querySelector(".menuLabel");
 
 /*Javascript above, all menu items with querySelectorAll. */
 
@@ -19,10 +20,12 @@ function toggleMenu() {
     menu.classList.remove("showMenu");
     closeIcon.style.display = "none";
     menuIcon.style.display = "block";
+    menuLabel.style.display = "block";
   } else {
     menu.classList.add("showMenu");
     closeIcon.style.display = "block";
     menuIcon.style.display = "none";
+    menuLabel.style.display = "none"
   }
 }
 

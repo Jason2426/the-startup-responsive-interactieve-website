8f899b57db63fc88d2d93cad2b342d3cec7b4adb
Jason <jay.jpy.wong@gmail.com>
2024-01-18 22:02:02 +0100

    Small styling fixes

--- scripts/script.js
+++ scripts/script.js
@@ -5,18 +5,23 @@ const closeIcon= document.querySelector(".closeIcon");
 const menuIcon = document.querySelector(".menuIcon");
 const menuLabel = document.querySelector(".menuLabel");
 
+// const hideItem = document.querySelector(".hideItem");
+
 function toggleMenu() {
   if (menu.classList.contains("showMenu")) {
     menu.classList.remove("showMenu");
     closeIcon.style.display = "none";
     menuIcon.style.display = "block";
     menuLabel.style.display = "block";
+
   } else {
     menu.classList.add("showMenu");
     closeIcon.style.display = "block";
     menuIcon.style.display = "none";
     menuLabel.style.display = "none";
+    // menuLabel.classList.toggle("hideItem");
   }
 }
 
 hamburger.addEventListener("click", toggleMenu);
+
